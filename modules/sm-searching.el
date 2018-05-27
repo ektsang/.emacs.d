304b5a7c5de2b96d43c0da2fa8de6c9d516158f4
George Hartzell <hartzell@alerce.com>
2018-05-27 12:00:37 -0700

    ag.el should reuse buffers, not create bazillions

--- modules/sm-searching.el
+++ modules/sm-searching.el
@@ -1,7 +1,9 @@
 ;; ag (silver surfer)
 (use-package ag
   :bind ("C-c s" . ag)
-  :config (setq ag-highlight-search t))
+  :config
+  (setq ag-highlight-search t)
+  (setq ag-reuse-buffers t))
 
 ;; anzu
 ;; Shows isearch results in mode-line and better query-replace.
