097c8b4568a6497e8451d50d167e0e2e4d86412b
hartzell <hartzell@alerce.com>
2018-11-30 09:59:09 -0800

    Update straight bootstrapping code

--- core/sm-modules.el
+++ core/sm-modules.el
@@ -1,8 +1,10 @@
 ;;; sm-modules.el --- Configures available modules and the package manager.
 
 ;; Bootstrap straight.
-(let ((bootstrap-file (concat user-emacs-directory "straight/repos/straight.el/bootstrap.el"))
-      (bootstrap-version 3))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
